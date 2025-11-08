5064ad6e8e24c7a1064020967f3cf59f5ab2ab3f
BrendaBraga12 <brendabraga.senior@gmail.com>
2025-11-08 10:40:51 +0000

    Update product.py

--- inventory_report/product.py
+++ inventory_report/product.py
@@ -10,11 +10,5 @@ class Product:
     serial_number:str
     storage_instructions:str
 
-    def __str__(self):
+    def __str__(self)->str:
         return f"The product {self.id} - {self.product_name}, with serial number {self.serial_number}, manufactured on {self.manufacturing_date}by the company {self.company_name}, valid until {self.expiration_date}, must be storage according to the following instructions:{self.storage_instructions}"
-
-
-if __name__=="__main__":
-    product1=Product("01","Farinha","Farinini","01-05-2021","02-06-2023","TY68 409C JJ43 AD1 PL2F","precisar ser armazenado em local protegido da luz.")
-
-print(product1)
