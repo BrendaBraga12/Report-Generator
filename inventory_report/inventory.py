e4eaf1fc55666f35e7f6020393dae545da425c09
BrendaBraga12 <brendabraga.senior@gmail.com>
2025-11-08 10:41:47 +0000

    Update inventory.py

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,12 +1,10 @@
 from typing import Optional
+from inventory_report.product import Product
 
 class Inventory():
-       total_active_tasks=0
-       def __init__(self,data,tasks:Optional[list[str]]=None):
-              self.data=data
-              self.tasks=tasks if tasks else[]
-              self.increase_amount_of_tasks(len(self.tasks))
+       total_active_tasks = 0
+       def __init__(self,data:Optional[list[Product]]=None)->None:
+              self.data=[] if data is None else data
 
-def add_data(self,task:str):
-       self.tasks.append(task)
-       self.increase_amount_task()
+       def add_data(self,data:list[Product]):
+              self.data.extend(data)
